e0e9a777fdbbda665a033333c550524f9f5355b9
neyctech <gzh.ding@outlook.com>
2018-10-22 19:38:28 +0800

    Initial commit

---
+++ P2335/P2335/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  P2335
+//
+//  Created by 丁国真 on 2018/10/22.
+//  Copyright © 2018 丁国真. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
