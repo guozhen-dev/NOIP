2362891412ab3d408bd1faea50751aaf897c6f6e
neyctech <gzh.ding@outlook.com>
2018-10-22 19:39:21 +0800

    Initial commit

--- P2335/P2335/main.cpp
+++ P2335/P2335/main.cpp
@@ -6,10 +6,47 @@
 //  Copyright © 2018 丁国真. All rights reserved.
 //
 
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int data[150][150];
+int ans[150][150];
+int x ,y ;
+inline int get_dis(int x1,int y1,int x2,int y2){
+    return (abs(x1-x2)+abs(y1-y2));
+}
+inline int min(int a, int b)
+{
+    return (a>b) ? b : a;
+}
+inline void check(int curx, int cury ){
+    if(data[curx][cury]==1) {
+        ans[curx][cury] = 0 ;
+        return;
+    }
+    for (register int i = 0 ; i < x ;i++){
+        for(register int j = 0 ; j < y ; j++){
+            if(data[i][j]) ans[curx][cury]=min(get_dis(curx, cury, i, j),ans[curx][cury]);
+            
+        }
+    }
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    cin >> x >> y ;
+    memset(ans,0x7f,sizeof(ans));
+    for (int i =0 ; i < x ; i++){
+        for(int j = 0 ; j < y ;j++){
+            cin >> data[i][j];
+        }
+    }
+    for (int i =0 ; i < x ; i++){
+        for(int j = 0 ; j < y ;j++){
+            check(i,j);
+            cout << ans[i][j]<<" ";
+        }
+        cout << endl;
+    }
     return 0;
 }
