49b240377fd37ee64d9ac677af8d6a89c1ded372
neyctech <gzh.ding@outlook.com>
2018-11-06 08:55:42 +0000

    Initial Commit

---
+++ P1025.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dp[207][10];
+int main(){
+	int n , k;
+	cin >> n >> k;
+	for (int i = 1 ; i <= n ; i++){
+		for (int j = 1 ; j <= k ; j++){
+			if ( i<j ) dp[i][j] = 0 ;
+			else if ( i==j ) dp[i][j] = 1 ;
+			else dp[i][j] = dp[i-1][j-1] + dp[i-j][j];
+		}
+	}
+	cout << dp[n][k]<<endl;
+	return 0 ;
+}
+
